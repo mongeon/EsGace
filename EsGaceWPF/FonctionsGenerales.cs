6a0fb07a7c5d952f8777da146190c014af214ca1
Gabriel <mongeon@users.noreply.github.com>
2009-01-26 03:17:58 +0000

    Corrected some minors bugs Added a new WPF version. Completed version is coming soon!

---
+++ EsGaceWPF/FonctionsGenerales.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+
+namespace EsGace.Classes
+{
+    class FonctionsGenerales
+    {
+        public static string TransformerTailleEnTexte(long taille)
+        {
+            Double TailleD = Convert.ToDouble(taille);
+            int dec = Convert.ToInt32(EsGaceWPF.Properties.Settings.Default.Decimal);
+            if (TailleD <= 1024)
+            {
+                return Math.Round(TailleD, dec).ToString() + " " + Traduction("Octet"); ;
+            }
+            else if (TailleD <= 1024 * 1024)
+            {
+                return Math.Round((TailleD / 1024), dec).ToString() + " " + Traduction("KiloOctet");
+            }
+            else if (TailleD <= 1024 * 1024 * 1024)
+            {
+                return Math.Round((TailleD / (1024 * 1024)), dec).ToString() + " " + Traduction("MegaOctet");
+            }
+            else
+            {
+                return Math.Round((TailleD / (1024 * 1024 * 1024)), dec).ToString() + " " + Traduction("GigaOctet");
+
+            }
+        }
+        public static string Traduction(string Texte)
+        {
+            try
+            {
+                ResourceManager LocRM = new ResourceManager("EsGaceWPF.ResourcesEsGace", Assembly.GetExecutingAssembly());
+                LocRM.IgnoreCase = true;
+                return LocRM.GetString(Texte);
+            }
+            catch (Exception)
+            {
+
+                return Texte;
+            }
+
+        }
+    }
+}
---
+++ EsGaceWPF/TreeViewClasses.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using EsGaceEngin;
+
+namespace EsGaceWPF
+{
+    #region HeaderToImageConverter
+
+    [ValueConversion(typeof(Item), typeof(bool))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BitmapImage source =null;
+            Uri uri;
+            switch ((value as Item).TypeItem)
+            {
+                case Item.eTypeItem.Indefini:
+                    break;
+                case Item.eTypeItem.Lecteur:
+                     uri = new Uri("pack://application:,,,/Images/drive.png");
+                     source = new BitmapImage(uri);
+                    
+                    break;
+                case Item.eTypeItem.Repertoire:
+                     uri = new Uri("pack://application:,,,/Images/folder.png");
+                     source = new BitmapImage(uri);
+                   
+                    break;
+                case Item.eTypeItem.Fichier:
+                     uri = new Uri("pack://application:,,,/Images/page.png");
+                     source = new BitmapImage(uri);
+                    
+                    break;
+
+            }
+            return source;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+
+    #endregion // DoubleToIntegerConverter
+
+
+}
